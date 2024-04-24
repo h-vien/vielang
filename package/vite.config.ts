56117fefb90d98c7154b1811dd6b5c7482d662a6
Vien Huynh <vien200203@gmail.com>
2024-04-24 22:14:55 +0700

    chore: bump up npm

--- package/vite.config.ts
+++ package/vite.config.ts
@@ -4,10 +4,27 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   resolve: {
     alias: {
-      "@parser": path.resolve("./src"),
+      '@parser': path.resolve('./src')
+    }
+  },
+  build: {
+    emptyOutDir: false,
+    lib: {
+      name: '@vietscript/parser',
+      entry: path.resolve('./src/index.ts'),
+      formats: ['es', 'cjs', 'umd'],
+      fileName: 'index'
     },
+    rollupOptions: {
+      external: [],
+
+      output: {
+        exports: 'named',
+        globals: {}
+      }
+    }
   },
   test: {
     globals: true
-  },
+  }
 })
