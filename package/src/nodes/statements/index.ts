92e6ff46f1be9881fe0626a052f280223fac3e5f
h-vien <vien200203@gmail.com>
2024-05-08 10:10:54 +0700

    feat(statments): implement if statement

--- package/src/nodes/statements/index.ts
+++ package/src/nodes/statements/index.ts
@@ -3,12 +3,18 @@ import { Parser } from '@parser/parser'
 import { BlockStatement } from './block'
 import { ExpressionStatement } from './expression'
 import { ReturnStatement } from './return'
+import { IfStatement } from './if'
 
 export class Statement {
   constructor(parser: Parser) {
     switch (parser.nextToken?.type) {
       case '{': {
         Object.assign(this, new BlockStatement(parser))
+        break
+      }
+      case Keyword.IF: {
+        console.log('go here?')
+        Object.assign(this, new IfStatement(parser))
         break
       }
       case Keyword.RETURN: {
