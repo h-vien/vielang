8d28ba68e61e02fd4486c33d0b3668ffacd8bcd4
h-vien <vien200203@gmail.com>
2024-05-08 10:10:54 +0700

    feat(playground): first looks

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,6 +1,8 @@
-import Editor from '@monaco-editor/react'
-import { Button, Card, Col, Row } from 'antd'
+import { LoadingOutlined } from '@ant-design/icons'
+import CodeEditor from '@uiw/react-textarea-code-editor'
 import { parser, transpiler } from '@vielang/parser'
+import { Button, Card, Col, Divider, Row } from 'antd'
+import axios from 'axios'
 import { useState } from 'react'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
@@ -8,15 +10,31 @@ import 'react-toastify/dist/ReactToastify.css'
 function App() {
   const [program, setProgram] = useState('')
   const [result, setResult] = useState('')
-  const onCompile = () => {
-    const code = 'khai báo tên = "Viên";'
-    const ast = parser.parse(code)
-    console.log(ast, '\n', '=======================')
+  const [isLoading, setIsLoading] = useState(false)
+  const onCompile = async () => {
     const _program = transpiler.compile(program)
-    console.log(_program)
-    setResult(_program.target)
+    const result = eval('let a = 1; console.log(a)').then((res) => console.log(res, 'this log res'))
+    console.log(result, 'result')
+    // try {
+    //   setIsLoading(true)
+    //   const res = await axios.post('https://emkc.org/api/v2/piston/execute', {
+    //     language: 'js',
+    //     version: '18.15.0',
+    //     files: [
+    //       {
+    //         content: _program.target
+    //       }
+    //     ]
+    //   })
+    //   setResult(res.data.run.output)
+    // } catch (err) {
+    //   console.log(err)
+    // } finally {
+    //   setIsLoading(false)
+    // }
   }
-  function handleEditorChange(value: any, event: any) {
+  function handleEditorChange(value: any) {
+    console.log(value)
     setProgram(value)
     // here is the current value
   }
@@ -25,17 +43,37 @@ function App() {
     <Card>
       <Row>
         <Col span={12}>
-          <Editor
-            onChange={handleEditorChange}
-            height='90vh'
-            defaultLanguage='javascript'
-            defaultValue='// some comment'
+          <CodeEditor
+            value={program}
+            language='js'
+            placeholder='Please enter JS code.'
+            onChange={(evn) => handleEditorChange(evn.target.value)}
+            padding={15}
+            style={{
+              backgroundColor: '#232D3F',
+              fontSize: 18,
+              height: '100vh',
+              fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace'
+            }}
           />
         </Col>
-        <Col span={2}>
-          <Button onClick={onCompile}>compile</Button>
+        <Col span={10} className='p-8 text-white'>
+          <Button onClick={onCompile} shape='default'>
+            Run Code
+          </Button>
+          {isLoading ? (
+            <LoadingOutlined />
+          ) : (
+            <>
+              {' '}
+              <Divider />
+              <h2>Result</h2>
+              <div className='border min-h-32 w-full p-4 mt-1 border-solid'>
+                {result ? result : 'Click "Run" to compile code'}
+              </div>
+            </>
+          )}
         </Col>
-        <Col span={10}>{JSON.parse(JSON.stringify(result))}</Col>
       </Row>
       {/* {routeElements} */}
       <ToastContainer
