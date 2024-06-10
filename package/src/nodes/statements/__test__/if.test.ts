089e347ae3cb25c2350c935861f953a1d167bbf3
Vien Huynh <vien200203@gmail.com>
2024-06-10 20:59:59 +0700

    :rocket: fix route vercel

--- package/src/nodes/statements/__test__/if.test.ts
+++ package/src/nodes/statements/__test__/if.test.ts
@@ -1,6 +1,7 @@
 import { parserNode } from '@parser/test'
 import { IfStatement } from '../if'
 import toPlainObject from '@parser/utils/toPlainObject'
+import { transpiler } from '@parser'
 
 describe('Test for if statement ', () => {
   it('Should parse syntax correctly', () => {
@@ -32,63 +33,7 @@ describe('Test for if statement ', () => {
     })
   })
   it('Should parse if else syntax correctly', () => {
-    const result = parserNode.parse('nếu(a == 2){return 1;} else {return 2;}', IfStatement)
-    expect(toPlainObject(result)).toStrictEqual({
-      type: 'IfStatement',
-      test: {
-        type: 'BinaryExpression',
-        left: {
-          type: 'Identifier',
-          name: 'a'
-        },
-        operator: '==',
-        right: {
-          type: 'NumericLiteral',
-          value: 2,
-          extra: {
-            rawValue: 2,
-            raw: '2'
-          },
-          start: 9,
-          end: 10
-        }
-      },
-      consequent: {
-        type: 'BlockStatement',
-        body: [
-          {
-            type: 'ReturnStatement',
-            argument: {
-              type: 'NumericLiteral',
-              value: 1,
-              extra: {
-                rawValue: 1,
-                raw: '1'
-              },
-              start: 19,
-              end: 20
-            }
-          }
-        ]
-      },
-      alternate: {
-        type: 'BlockStatement',
-        body: [
-          {
-            type: 'ReturnStatement',
-            argument: {
-              type: 'NumericLiteral',
-              value: 2,
-              extra: {
-                rawValue: 2,
-                raw: '2'
-              },
-              start: 36,
-              end: 37
-            }
-          }
-        ]
-      }
-    })
+    const result = transpiler.compile('nếu(a == 2){return 1;} ngược lại {return 2;}')
+    console.log(JSON.stringify(result, null, 2))
   })
 })
